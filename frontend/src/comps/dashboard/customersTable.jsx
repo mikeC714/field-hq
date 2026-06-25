34aa5bac42762c741872c61aa16be23bd504f38b
Mike Carter <carter71404@icloud.com>
2026-06-25 13:19:42 -0400

    working on create quote form for desktop

--- frontend/src/comps/dashboard/customersTable.jsx
+++ frontend/src/comps/dashboard/customersTable.jsx
@@ -79,6 +79,15 @@ export function CustomerTable({ data, page, setPage, handleDelete, visible, setV
 											<Trash2 size={20} />
 										</button>
 								)}
+									<button 
+										className='customerDeleteBtn'
+										onClick={(e) => {
+											e.stopPropagation();
+											handleDelete({ quoteId: quote?.id })
+										}}
+										>
+											<Trash2 size={20} />
+										</button>
                                         <div className="trLeft">
 											<div className="customerJobId">QT-{String(customerIndex + 1).padStart(3,0)}</div>
 												<div className="customerNameNAdd">
@@ -102,7 +111,7 @@ export function CustomerTable({ data, page, setPage, handleDelete, visible, setV
 							console.log("PAGE MINUS 1")
 							setPage(p => p -1)}}
                     >
-                        <ArrowLeft />
+                        <ArrowLeft size={18}/>
                     </button>
                     <button 
                         disabled = {page?.nextPage ? false : true }
@@ -110,7 +119,7 @@ export function CustomerTable({ data, page, setPage, handleDelete, visible, setV
 							console.log("PAGE PLUS 1")
 							setPage(p => p +1)}}
                     >
-                        <ArrowRight />
+                        <ArrowRight size={18}/>
                     </button>
                 </div>
             </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './styles/auth.css';
 import './styles/alerts.css';
 import './styles/profile.css';
 import './styles/dashboard.css';
+import './styles/createquote.css';
 
 
 import { App } from './App.jsx'
